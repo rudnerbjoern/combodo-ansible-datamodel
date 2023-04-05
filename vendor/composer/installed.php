8bc7898e51c78680e60de672bb3f896794a21a2a
xtophe38 <christophe.naud@gmail.com>
2023-04-05 17:59:20 +0200

    Add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'dbd5eac568a8052afa3f383f8761a812756330a1',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'dbd5eac568a8052afa3f383f8761a812756330a1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
